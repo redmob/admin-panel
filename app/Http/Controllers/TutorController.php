e888915490b06213ac4986cf3f3cfe84989f5f4e
redmob <itsmealdar@gmail.com>
2022-11-07 11:56:38 +0530

    Sortable

--- app/Http/Controllers/TutorController.php
+++ app/Http/Controllers/TutorController.php
@@ -19,9 +19,9 @@ class TutorController extends Controller
             return Datatables::of($data)
                 ->addIndexColumn()
 //                ->orderColumns(['name', 'email','last_active_datetime'], '-:column $1')
-                ->orderColumn('selected_plan', function ($query, $order) {
-                    $query->orderBy('selected_plan', $order);
-                })
+//                ->orderColumn('selected_plan', function ($query, $order) {
+//                    $query->orderBy('selected_plan', $order);
+//                })
                 ->addColumn('selected_plan', function ($row) {
                     if ($row->selected_plan == "FREE") {
                         return 'FREE';
