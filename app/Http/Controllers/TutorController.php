d6559651633cd2f5950951306aacb8a9cdccea10
redmob <itsmealdar@gmail.com>
2023-01-07 18:55:50 +0530

    storage added

--- app/Http/Controllers/TutorController.php
+++ app/Http/Controllers/TutorController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use DataTables;
 
 
